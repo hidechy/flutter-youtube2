a761360cae1fbfc5fce88f5c47d53ba32b66cd0a
Hide Toyoda <hide.toyoda@gmail.com>
2022-04-13 14:06:25 +0900

    use freezed

--- lib/screens/bunrui_list_screen.dart
+++ lib/screens/bunrui_list_screen.dart
@@ -1,84 +1,42 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-//import 'package:youtubeplayer/screens/youtube_play_screen.dart';
-
 import 'dart:convert';
 
-import '../model/youtube_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart';
+
+import '../data/bunrui_list_state.dart';
+
 import '../model/video.dart';
+import '../model/youtube_data.dart';
 
 import '../utilities/utility.dart';
 
-import '../logic/logic.dart';
 import 'home_screen.dart';
 
-class BunruiListScreen extends StatefulWidget {
+import 'components/video_list_item.dart';
+
+class BunruiListScreen extends ConsumerWidget {
+  BunruiListScreen({Key? key, required this.bunrui}) : super(key: key);
+
   final String bunrui;
 
-  const BunruiListScreen({Key? key, required this.bunrui}) : super(key: key);
-
-  @override
-  _BunruiListScreenState createState() => _BunruiListScreenState();
-}
-
-class _BunruiListScreenState extends State<BunruiListScreen> {
   final Utility _utility = Utility();
-  final _logic = Logic();
 
-  List<Video> _youtube = [];
+  late WidgetRef _ref;
 
-  final List<String> _selectedList = [];
+  late BuildContext _context;
 
-  final List<String> _specialList = [];
-
-  /// 初期動作
   @override
-  void initState() {
-    super.initState();
+  Widget build(BuildContext context, WidgetRef ref) {
+    _ref = ref;
 
-    _makeDefaultDisplayData();
-  }
+    _context = context;
 
-  /// 初期データ作成
-  void _makeDefaultDisplayData() async {
-    ////////////////////////////////////////
-    String url = "http://toyohide.work/BrainLog/api/getYoutubeList";
-    Map<String, String> headers = {'content-type': 'application/json'};
+    final state = ref.watch(bunruiListProvider(bunrui));
 
-    String body = '';
-    switch (widget.bunrui) {
-      case 'all':
-        body = json.encode({"bunrui": ''});
-        break;
-
-      default:
-        body = json.encode({"bunrui": widget.bunrui});
-        break;
-    }
-
-    Response response =
-        await post(Uri.parse(url), headers: headers, body: body);
-
-    final youtubeData = youtubeDataFromJson(response.body);
-    _youtube = youtubeData.data;
-    ////////////////////////////////////////
-    for (var element in _youtube) {
-      if (element.special == '1') {
-        _specialList.add(element.youtubeId);
-      }
-    }
-
-    setState(() {});
-  }
-
-  ///
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.bunrui),
+        title: Text(bunrui),
         backgroundColor: Colors.redAccent.withOpacity(0.3),
         centerTitle: true,
 
@@ -99,10 +57,13 @@ class _BunruiListScreenState extends State<BunruiListScreen> {
       body: Stack(
         fit: StackFit.expand,
         children: [
-          _utility.getBackGround(context: context),
+          _utility.getBackGround(),
           Column(
             children: [
-              Expanded(child: _movieList()),
+              if (state.youtubeList.isNotEmpty)
+                Expanded(
+                  child: _movieList(videoList: state.youtubeList),
+                ),
               const Divider(
                 color: Colors.redAccent,
                 thickness: 3,
@@ -151,28 +112,24 @@ class _BunruiListScreenState extends State<BunruiListScreen> {
   }
 
   ///
-  Widget _movieList() {
+  Widget _movieList({required List<Video> videoList}) {
     return ListView.separated(
-      itemCount: _youtube.length,
-      itemBuilder: (context, int position) => _listItem(position: position),
-      separatorBuilder: (BuildContext context, int index) {
+      itemCount: videoList.length,
+      itemBuilder: (context, int position) =>
+          _listItem(video: videoList[position]),
+      separatorBuilder: (_, __) {
         return const Divider(color: Colors.white);
       },
     );
   }
 
-  /// リストアイテム表示
-  Widget _listItem({required int position}) {
-    var date = _youtube[position].getdate;
-    var year = date.substring(0, 4);
-    var month = date.substring(4, 6);
-    var day = date.substring(6);
-
+  ///
+  Widget _listItem({required Video video}) {
     return Card(
-      color: _getSelectedBgColor(youtubeId: _youtube[position].youtubeId),
+      color: _getSelectedBgColor(youtubeId: video.youtubeId),
       child: ListTile(
         leading: GestureDetector(
-          onTap: () => _addSelectedAry(youtubeId: _youtube[position].youtubeId),
+          onTap: () => _addSelectedAry(youtubeId: video.youtubeId),
           child: const Icon(
             Icons.control_point,
             color: Colors.white,
@@ -180,84 +137,18 @@ class _BunruiListScreenState extends State<BunruiListScreen> {
         ),
         title: DefaultTextStyle(
           style: const TextStyle(fontSize: 12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 180,
-                    child: FadeInImage.assetNetwork(
-                      placeholder: 'assets/images/no_image.png',
-                      image:
-                          'https://img.youtube.com/vi/${_youtube[position].youtubeId}/mqdefault.jpg',
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: (_youtube[position].special == '1')
-                        ? const Icon(
-                            Icons.star,
-                            color: Colors.greenAccent,
-                          )
-                        : Icon(
-                            Icons.check_box_outline_blank,
-                            color: Colors.black.withOpacity(0.2),
-                          ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.topRight,
-                      child: GestureDetector(
-                        onTap: () => _openBrowser(
-                            youtubeId: _youtube[position].youtubeId),
-                        child: const Icon(Icons.link),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(_youtube[position].title),
-                  const SizedBox(height: 5),
-                  Text.rich(
-                    TextSpan(children: [
-                      TextSpan(text: _youtube[position].youtubeId),
-                      const TextSpan(text: ' / '),
-                      TextSpan(
-                        text: _youtube[position].playtime,
-                        style: const TextStyle(color: Colors.yellowAccent),
-                      ),
-                    ]),
-                  ),
-                  const SizedBox(height: 5),
-                  Container(
-                    alignment: Alignment.topRight,
-                    child: Text(_youtube[position].channelTitle),
-                  ),
-                  const SizedBox(height: 5),
-                  Container(
-                    alignment: Alignment.topRight,
-                    child: Text.rich(
-                      TextSpan(children: [
-                        TextSpan(text: '$year-$month-$day'),
-                        const TextSpan(text: ' / '),
-                        TextSpan(
-                          text: _youtube[position].pubdate,
-                          style: const TextStyle(
-                            color: Colors.yellowAccent,
-                          ),
-                        ),
-                      ]),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+          child: VideoListItem(
+            data: Video(
+              youtubeId: video.youtubeId,
+              title: video.title,
+              url: video.url,
+              channelId: video.channelId,
+              channelTitle: video.channelTitle,
+              playtime: video.playtime,
+              getdate: video.getdate,
+              pubdate: video.pubdate,
+              special: video.special,
+            ),
           ),
         ),
       ),
@@ -265,19 +156,15 @@ class _BunruiListScreenState extends State<BunruiListScreen> {
   }
 
   ///
-  void _addSelectedAry({youtubeId}) {
-    if (_selectedList.contains(youtubeId)) {
-      _selectedList.remove(youtubeId);
-    } else {
-      _selectedList.add(youtubeId);
-    }
-
-    setState(() {});
+  void _addSelectedAry({required String youtubeId}) {
+    final viewModel = _ref.watch(bunruiListProvider(bunrui).notifier);
+    viewModel.addSelectedAry(youtubeId: youtubeId);
   }
 
   ///
-  Color _getSelectedBgColor({youtubeId}) {
-    if (_selectedList.contains(youtubeId)) {
+  Color _getSelectedBgColor({required String youtubeId}) {
+    final state = _ref.watch(bunruiListProvider(bunrui));
+    if (state.selectedList.contains(youtubeId)) {
       return Colors.greenAccent.withOpacity(0.3);
     } else {
       return Colors.black.withOpacity(0.1);
@@ -285,98 +172,176 @@ class _BunruiListScreenState extends State<BunruiListScreen> {
   }
 
   ///
-  void _uploadDeleteItems() async {
-    bool _backHomeScreen = false;
+  void _uploadSpecialItems() async {
+    final state = _ref.watch(bunruiListProvider(bunrui));
+    final viewModel = _ref.watch(bunruiListProvider(bunrui).notifier);
 
-    if (_selectedList.isNotEmpty) {
+    if (state.selectedList.isNotEmpty) {
       var _list = [];
-      for (var element in _selectedList) {
-        _list.add("'$element'");
+
+      for (var element in state.selectedList) {
+        var sp = (state.specialList.contains(element)) ? 0 : 1;
+        _list.add("$element|$sp");
       }
 
-      if (_youtube.length == _list.length) {
-        _backHomeScreen = true;
-      }
-
-      await _logic.uploadBunruiItems(
-        bunrui: 'delete',
+      viewModel.uploadBunruiItems(
+        flag: 'special',
         bunruiItems: _list,
+        bunrui: bunrui,
       );
     }
-
-    (_backHomeScreen) ? _goHomeScreen() : _goBunruiListScreen();
   }
 
   ///
   void _uploadEraseItems() async {
-    bool _backHomeScreen = false;
+    final state = _ref.watch(bunruiListProvider(bunrui));
+    final viewModel = _ref.watch(bunruiListProvider(bunrui).notifier);
 
-    if (_selectedList.isNotEmpty) {
+    if (state.selectedList.isNotEmpty) {
+      bool _backHomeScreen = false;
+
       var _list = [];
-      for (var element in _selectedList) {
+      for (var element in state.selectedList) {
         _list.add("'$element'");
       }
 
-      if (_youtube.length == _list.length) {
+      if (state.youtubeList.length == _list.length) {
         _backHomeScreen = true;
       }
 
-      await _logic.uploadBunruiItems(
-        bunrui: 'erase',
+      viewModel.uploadBunruiItems(
+        flag: 'erase',
         bunruiItems: _list,
+        bunrui: bunrui,
       );
+
+      if (_backHomeScreen) {
+        _goHomeScreen();
+      }
     }
-
-    (_backHomeScreen) ? _goHomeScreen() : _goBunruiListScreen();
   }
 
   ///
-  void _openBrowser({required String youtubeId}) async {
-    var url = 'https://youtu.be/$youtubeId';
+  void _uploadDeleteItems() async {
+    final state = _ref.watch(bunruiListProvider(bunrui));
+    final viewModel = _ref.watch(bunruiListProvider(bunrui).notifier);
 
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {}
-  }
+    if (state.selectedList.isNotEmpty) {
+      bool _backHomeScreen = false;
 
-  ///
-  void _uploadSpecialItems() async {
-    if (_selectedList.isNotEmpty) {
       var _list = [];
-      for (var element in _selectedList) {
-        var sp = (_specialList.contains(element)) ? 0 : 1;
-
-        _list.add("$element|$sp");
+      for (var element in state.selectedList) {
+        _list.add("'$element'");
       }
 
-      await _logic.uploadBunruiItems(
-        bunrui: 'special',
+      if (state.youtubeList.length == _list.length) {
+        _backHomeScreen = true;
+      }
+
+      viewModel.uploadBunruiItems(
+        flag: 'delete',
         bunruiItems: _list,
+        bunrui: bunrui,
       );
+
+      if (_backHomeScreen) {
+        _goHomeScreen();
+      }
     }
-
-    _goBunruiListScreen();
-  }
-
-  ///////////////////////////////////////////////////////
-  void _goBunruiListScreen() {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (_) => BunruiListScreen(
-          bunrui: widget.bunrui,
-        ),
-      ),
-    );
   }
 
   ///
   void _goHomeScreen() {
     Navigator.pushReplacement(
-      context,
+      _context,
       MaterialPageRoute(
         builder: (_) => HomeScreen(),
       ),
     );
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+final bunruiListProvider = StateNotifierProvider.autoDispose
+    .family<BunruiListStateNotifier, BunruiListState, String>((ref, bunrui) {
+  return BunruiListStateNotifier()..getVideoData(bunrui: bunrui);
+});
+
+//////////////////////////////////////////////////////////////////////////
+
+class BunruiListStateNotifier extends StateNotifier<BunruiListState> {
+  BunruiListStateNotifier()
+      : super(
+          const BunruiListState(
+            youtubeList: [],
+            specialList: [],
+            selectedList: [],
+          ),
+        );
+
+  ///
+  void getVideoData({required String bunrui}) async {
+    try {
+      String url = "http://toyohide.work/BrainLog/api/getYoutubeList";
+      Map<String, String> headers = {'content-type': 'application/json'};
+      String body = json.encode({"bunrui": bunrui});
+      Response response =
+          await post(Uri.parse(url), headers: headers, body: body);
+      final youtubeData = youtubeDataFromJson(response.body);
+
+      //-----------------------------------//
+      final specialList = <String>[];
+      for (var element in youtubeData.data) {
+        if (element.special == '1') {
+          specialList.add(element.youtubeId);
+        }
+      }
+      //-----------------------------------//
+
+      state = state.copyWith(
+        youtubeList: youtubeData.data,
+        specialList: specialList,
+      );
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+
+  ///
+  void addSelectedAry({required String youtubeId}) {
+    List<String> _list = [...state.selectedList];
+
+    if (_list.contains(youtubeId)) {
+      _list.remove(youtubeId);
+    } else {
+      _list.add(youtubeId);
+    }
+
+    state = state.copyWith(selectedList: _list);
+  }
+
+  ///
+  void uploadBunruiItems({
+    required String flag,
+    required List bunruiItems,
+    required String bunrui,
+  }) async {
+    Map<String, dynamic> _uploadData = {};
+    _uploadData['bunrui'] = flag;
+    _uploadData['youtube_id'] = bunruiItems.join(',');
+
+    String url = "http://toyohide.work/BrainLog/api/bunruiYoutubeData";
+    Map<String, String> headers = {'content-type': 'application/json'};
+    String body = json.encode(_uploadData);
+    await post(Uri.parse(url), headers: headers, body: body);
+
+    state = state.copyWith(selectedList: []);
+
+    getVideoData(bunrui: bunrui);
   }
 }
