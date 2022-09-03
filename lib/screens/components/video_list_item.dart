0da8ebbd997f6051c4f251a4eff9ea3c4409133f
toyohide <hide.toyoda@gmail.com>
2022-09-03 17:26:42 +0900

    last played at

--- lib/screens/components/video_list_item.dart
+++ lib/screens/components/video_list_item.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../model/video.dart';
 
-class VideoListItem extends StatelessWidget {
-  const VideoListItem({Key? key, required this.data, required this.linkDisplay})
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class VideoListItem extends ConsumerWidget {
+  VideoListItem({Key? key, required this.data, required this.linkDisplay})
       : super(key: key);
 
   final Video data;
   final bool linkDisplay;
 
+  late WidgetRef _ref;
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    _ref = ref;
+
     var year = data.getdate.substring(0, 4);
     var month = data.getdate.substring(4, 6);
     var day = data.getdate.substring(6);
@@ -143,10 +152,39 @@ class VideoListItem extends StatelessWidget {
 
   ///
   void _openBrowser({required String youtubeId}) async {
+    final updateVideoPlayedAtViewModel =
+        _ref.watch(updateVideoPlayedAtProvider.notifier);
+
     var url = 'https://youtu.be/$youtubeId';
 
     if (await canLaunch(url)) {
+      updateVideoPlayedAtViewModel.updateVideoPlayedAt(youtubeId: youtubeId);
+
       await launch(url);
     } else {}
+  }
+}
+
+/////////////////////////////////////////////////////////////
+
+final updateVideoPlayedAtProvider =
+    StateNotifierProvider.autoDispose<UpdateVideoPlayedAtStateNotifier, int>(
+        (ref) {
+  return UpdateVideoPlayedAtStateNotifier(0);
+});
+
+class UpdateVideoPlayedAtStateNotifier extends StateNotifier<int> {
+  UpdateVideoPlayedAtStateNotifier(int state) : super(state);
+
+  ///
+  void updateVideoPlayedAt({required String youtubeId}) async {
+    Map<String, dynamic> _uploadData = {};
+    _uploadData['youtube_id'] = youtubeId;
+    _uploadData['date'] = DateTime.now().toString().split(' ')[0];
+
+    String url = "http://toyohide.work/BrainLog/api/updateVideoPlayedAt";
+    Map<String, String> headers = {'content-type': 'application/json'};
+    String body = json.encode(_uploadData);
+    await post(Uri.parse(url), headers: headers, body: body);
   }
 }
